42e58ed43ecaf7aec956faf6114b1922a369188c
Orseni <orseni@softbox.com.br>
2018-03-19 03:28:30 -0300

    .

--- pydbwrapper/database.py
+++ pydbwrapper/database.py
@@ -11,8 +11,7 @@ VERSION = "1.0.1"
 QUERIES_DIR = os.path.realpath(os.path.curdir) + '/sql/'
 
 global pool
-pool = psycopg2.pool.SimpleConnectionPool(50, 50, **self.config.data)
-
+pool = None
 
 class DictWrapper(object):
     """Dict wrapper to access dict attributes with . operator"""
@@ -116,6 +115,9 @@ class Database(object):
     def __init__(self):
         self.config = Config.instance()
         #self.connection = psycopg2.connect(**self.config.data)
+        if pool == None:
+            pool = psycopg2.pool.SimpleConnectionPool(50, 50, **self.config.data)
+
         self.connection = pool.getconn()
 
     def __enter__(self):
