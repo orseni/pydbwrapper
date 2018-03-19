863628c216c8763836a10556589d86c134710f57
Orseni <orseni@softbox.com.br>
2018-03-18 22:50:30 -0300

    .

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+
+from setuptools import setup, find_packages
+
+import pg_simple
+
+try:
+    with open(os.path.abspath('./README.md')) as stream:
+        long_description = stream.read()
+except:
+    long_description = 'pydbwrapper is a simple wrapper for Python psycopg2 with connection pooling'
+
+setup(
+    name=pg_simple.__name__,
+    version=pg_simple.VERSION,
+    packages=find_packages(),
+    install_requires=['psycopg2'],
+    classifiers=['Topic :: Database',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.6',
+                 'Programming Language :: Python :: 2.7',
+                 'License :: OSI Approved :: BSD License',
+                 'Operating System :: OS Independent',
+                 'Intended Audience :: Developers',
+                 'Development Status :: 3 - Alpha'],
+    author='Alexandre Fidelis',
+    author_email='orseni@gmail.com',
+    description='pydbwrapper is a simple wrapper for Python psycopg2 with connection pooling',
+    long_description=long_description,
+    license='BSD',
+    keywords='psycopg2 postgresql sql database',
+    url='https://github.com/masroore/pg_simple',
+)
